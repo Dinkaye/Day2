70b91154d39cd1e5f11be6bf3ff079282900e682
Yeabsira Tesfaye <122875354+Dinkaye@users.noreply.github.com>
2023-11-23 14:41:49 +0300

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,15 @@
+//exercises 1
+let k  ='You cannot end a sentence with because because because is a conjunction ';
+console.log(k.lastIndexOf('because'))//18.use lastindexof to find the position of because
+let x ='30 days of javascript';
+console.log(x.repeat(2));
+//exercise 2
+let y ='i hope this course is not full of jargon';
+console.log(y.includes('jargon'));//6.check if jargon is there 
+console.log('1 1 1 1 1 \n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125');
+let h= 'you cannot end a sentence with because because because is a conjunction';//12.use subst()
+console.log(h.substr(30,25));
+//exercise 3
+let u='you cannot end a sentence with because because because is a conjunction';
+console.log(u.match('because'));
+
